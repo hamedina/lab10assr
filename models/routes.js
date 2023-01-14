5076ab949916beb7fc922e0f338f62ef780c5f8f
alperalt1 <alperalt@espol.edu.ec>
2023-01-14 17:04:43 -0500

    first commit

---
+++ models/routes.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://alperalt:final12.@cluster0.kwbimey.mongodb.net/router_db', {
+    useNewUrlParser: true,
+},
+(err) => {
+    if (!err) {
+        console.log('Connection suceeded');
+    } else {
+        console.log('Error in connection' + err);
+    }
+});
+
+require('./routes.model');
